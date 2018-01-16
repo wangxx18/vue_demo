cdf6f0b740a1fbad6e8077c37ac836f5af6c3043
wangxu321 <353188971@qq.com>
2018-01-16 17:35:50 +0800

    修改首页结构

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,35 +1,199 @@
 <template>
   <section>
-    <div id="content">
-      <div class="panel">
-        <div class="header topic_header">
+    <div id="main">
+      <div>
+        <div id="sidebar">
+          <div class="panel">
+            <div class="inner">
+              <p>CNode：Node.js专业中文社区</p>
+              <div>
+                您可以
+                <a href="/signin">登录</a>
+                或
+                <a href="/signup">注册</a>
+                , 也可以
+                <a href="/auth/github">
+          <span class="span-info">
+            通过 GitHub 登录
+          </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="panel">
+            <div class="inner ads">
+              <a href="https://alinode.aliyun.com/?ref=cnode" target="_blank" class="banner sponsor_outlink" data-label="alinode">
+                <img src="//dn-cnode.qbox.me/Fn4D6BhOTz1IswvmzeZ1q7QW1ls_">
+              </a>
+              <div class="sep10"></div>
+              <a href="http://www.ucloud.cn/site/active/gift.html?utm_source=cnodejs&amp;utm_medium=content_pic_pc&amp;utm_campaign=multicloud&amp;utm_content=gift&amp;ytag=cnodejs" target="_blank" class="banner sponsor_outlink" data-label="ucloud-banner">
+                <img src="//dn-cnode.qbox.me/FgQS-GQDfqwAD_v0NuhyYUOUk5MG">
+              </a>
+              <div class="sep10"></div>
+              <a href="https://0x7.me/UDyj" target="_blank" class="banner sponsor_outlink" data-label="qiniu-sidebar">
+                <img src="//dn-cnode.qbox.me/Fv9R31Y6ySKKJi95ldk9TRkJ7o5O">
+              </a>
+            </div>
+          </div>
+          <div class="panel">
+            <div class="header">
+              <span class="col_fade">无人回复的话题</span>
+            </div>
+            <div class="inner">
+
+              <ul class="unstyled">
+                <li>
+                  <div><a class="dark topic_title" href="/topic/5a5dbfa6afa0a121784a8c70" title="基于 React 和 antd 的 cnode web 客户端">基于 React 和 antd 的 cnode web 客户端</a>
+                  </div>
+                </li>
+                <li>
+                  <div><a class="dark topic_title" href="/topic/5a5db8c7a3692d014f4f14e2" title="有用thinkjs2做后台的吗">有用thinkjs2做后台的吗</a>
+                  </div>
+                </li>
+                <li>
+                  <div><a class="dark topic_title" href="/topic/5a5db4c09d371d4a059eeb22" title="Material Design 设计风格的博客上线了">Material Design 设计风格的博客上线了</a>
+                  </div>
+                </li>
+                <li>
+                  <div><a class="dark topic_title" href="/topic/5a5dae98afa0a121784a8c60" title="Nuxt+Express+Mysql整合实践写的一个博客平台，对于vue服务端渲染感兴趣的可以参考">Nuxt+Express+Mysql整合实践写的一个博客平台，对于vue服务端渲染感兴趣的可以参考</a>
+                  </div>
+                </li>
+                <li>
+                  <div><a class="dark topic_title" href="/topic/5a5d6b67afa0a121784a8c29" title="node源码_http_common.js 中是如何处理请求体的？">node源码_http_common.js 中是如何处理请求体的？</a>
+                  </div>
+                </li>
+
+              </ul>
+
+            </div>
+          </div>
+          <div class="panel">
+            <div class="header">
+              <span class="col_fade">积分榜</span>
+              &nbsp;
+              <a class="dark" href="/users/top100">TOP 100 &gt;&gt;</a>
+            </div>
+            <div class="inner">
+              <ol>
+                <li>
+                  <span class="top_score">20725</span>
+                  <span class="user_name"><a href="/user/i5ting">i5ting</a></span>
+                </li>
+                <li>
+                  <span class="top_score">14770</span>
+                  <span class="user_name"><a href="/user/alsotang">alsotang</a></span>
+                </li>
+                <li>
+                  <span class="top_score">9330</span>
+                  <span class="user_name"><a href="/user/leapon">leapon</a></span>
+                </li>
+                <li>
+                  <span class="top_score">8720</span>
+                  <span class="user_name"><a href="/user/jiyinyiyong">jiyinyiyong</a></span>
+                </li>
+                <li>
+                  <span class="top_score">6800</span>
+                  <span class="user_name"><a href="/user/yakczh">yakczh</a></span>
+                </li>
+                <li>
+                  <span class="top_score">6060</span>
+                  <span class="user_name"><a href="/user/imhered">imhered</a></span>
+                </li>
+                <li>
+                  <span class="top_score">5940</span>
+                  <span class="user_name"><a href="/user/DevinXian">DevinXian</a></span>
+                </li>
+                <li>
+                  <span class="top_score">5295</span>
+                  <span class="user_name"><a href="/user/magicdawn">magicdawn</a></span>
+                </li>
+                <li>
+                  <span class="top_score">4740</span>
+                  <span class="user_name"><a href="/user/captainblue2013">captainblue2013</a></span>
+                </li>
+                <li>
+                  <span class="top_score">4495</span>
+                  <span class="user_name"><a href="/user/fengmk2">fengmk2</a></span>
+                </li>
+              </ol>
+            </div>
+          </div>
+          <div class="panel">
+            <div class="header">
+              <span class="col_fade">友情社区</span>
+            </div>
+            <div class="inner">
+              <ol class="friendship-community">
+                <li>
+                  <a href="https://ruby-china.org/" target="_blank">
+                    <img src="//o4j806krb.qnssl.com/public/images/ruby-china-20150529.png">
+                  </a>
+                </li>
+                <div class="sep10"></div>
+                <li>
+                  <a href="http://golangtc.com/" target="_blank">
+                    <img src="//o4j806krb.qnssl.com/public/images/golangtc-logo.png">
+                  </a>
+                </li>
+                <div class="sep10"></div>
+                <li>
+                  <a href="http://phphub.org/" target="_blank">
+                    <img src="//o4j806krb.qnssl.com/public/images/phphub-logo.png">
+                  </a>
+                </li>
+                <div class="sep10"></div>
+                <li>
+                  <a href="https://testerhome.com/" target="_blank">
+                    <img src="//dn-cnode.qbox.me/FjLUc7IJ2--DqS706etPQ1EGajxK">
+                  </a>
+                </li>
+              </ol>
+            </div>
+          </div>
+          <div class="panel">
+            <div class="header">
+              <span class="col_fade">客户端二维码</span>
+            </div>
+            <div class="inner cnode-app-download">
+              <img width="200" src="//dn-cnode.qbox.me/FtG0YVgQ6iginiLpf9W4_ShjiLfU">
+              <br>
+              <a href="https://github.com/soliury/noder-react-native" target="_blank">客户端源码地址</a>
+            </div>
+          </div>
+        </div>
+
+        <div id="content">
+          <div class="panel">
+            <div class="header topic_header">
       <span class="topic_full_title">
         <span class="put_top" v-show=" list.top">置顶</span>
         {{list.title}}
       </span>
-          <div class="changes">
+              <div class="changes">
         <span>
           发布于 {{list.create_at | lastTime}}
         </span>
-            <span>
+                <span>
           作者 {{user}}
         </span>
-            <span>
+                <span>
           {{list.visit_count}} 次浏览
         </span>
 
-            <span>
+                <span>
             最后一次编辑是 {{list.last_reply_at | lastTime}}
           </span>
-            <span> 来自 {{list.tab | type}}</span>
+                <span> 来自 {{list.tab | type}}</span>
+              </div>
+            </div>
+            <div class="inner topic">
+              <div class="topic_content" v-html="list.content">
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="inner topic">
-          <div class="topic_content" v-html="list.content">
-          </div>
+
         </div>
       </div>
-
     </div>
   </section>
 </template>
